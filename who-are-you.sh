88ce1c8799347f0a3808cd07fcf35479687ef8ac
melfihry <fihrylara@gmail.com>
2024-01-08 17:21:45 +0000

    who-are-you.sh

--- who-are-you.sh
+++ who-are-you.sh
@@ -2,4 +2,5 @@
 jq_url="https://learn.zone01oujda.ma/assets/superhero/all.json"
 subject_id=70
 name=$(curl -s "$jq_url" | jq -r ".[] | select(.id == $subject_id) | .name")
-echo "$name"
+res='"'"$name"'"'
+echo "$res"
