ae73c79f5da7c250fdad5237610d233e6da48f95
melfihry <fihrylara@gmail.com>
2024-01-09 14:26:48 +0000

    myfamily.sh

--- myfamily.sh
+++ myfamily.sh
@@ -1,3 +1,3 @@
 #!bin/bash
 Api_curl="https://learn.zone01oujda.ma/assets/superhero/all.json"
-curl -s "$Api_curl" | jq -r ".[] | select(.id == $HERO_ID) | .connections.relatives"
+curl -s "$Api_curl" | jq -r -c ".[] | select(.id == $HERO_ID) | .connections.relatives"
