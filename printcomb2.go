9e1592d4719b8f8d866d323c3beb122943141d68
melfihry <fihrylara@gmail.com>
2024-01-10 14:49:49 +0000

    printcomb2.go

--- printcomb2.go
+++ printcomb2.go
@@ -8,16 +8,16 @@ func PrintComb2() {
 	for a := 0; a <= 98; a++ {
 		for b := 1; b <= 99; b++ {
 			last := !(a == 98 && b == 99)
-			z01.PrintRune(rune('0' + a / 10))
-			z01.PrintRune(rune('0' + a % 10))
+			z01.PrintRune(rune('0' + a/10))
+			z01.PrintRune(rune('0' + a%10))
 			z01.PrintRune(' ')
-			z01.PrintRune(rune('0'+ b / 10))
-			z01.PrintRune(rune('0'+ b % 10))
+			z01.PrintRune(rune('0' + b/10))
+			z01.PrintRune(rune('0' + b%10))
 			if last {
 				z01.PrintRune(',')
 				z01.PrintRune(' ')
 			}
 		}
 	}
+	z01.PrintRune('\n')
 }
-
