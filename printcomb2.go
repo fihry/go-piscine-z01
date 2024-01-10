5c6df5c7aa9572e5efb8563861cb7beb878fcddf
melfihry <fihrylara@gmail.com>
2024-01-10 16:38:59 +0000

    printcomb2.go

--- printcomb2.go
+++ printcomb2.go
@@ -1,23 +1,33 @@
-package piscine
+package main
 
 import (
 	"github.com/01-edu/z01"
 )
 
 func PrintComb2() {
-	for a := 0; a <= 98; a++ {
-		for b := 1; b <= 99; b++ {
-			last := !(a == 98 && b == 99)
-			z01.PrintRune(rune('0' + a/10))
-			z01.PrintRune(rune('0' + a%10))
-			z01.PrintRune(' ')
-			z01.PrintRune(rune('0' + b/10))
-			z01.PrintRune(rune('0' + b%10))
-			if last {
-				z01.PrintRune(',')
-				z01.PrintRune(' ')
+	for a := '0'; a <= '9'; a++ {
+		for b := '0'; b <= '9'; b++ {
+			for c := '0'; c <= '9'; c++ {
+				for d := '0'; d <= '9'; d++ {
+					if a < c || (a == c && b < d) {
+						z01.PrintRune(a)
+						z01.PrintRune(b)
+						z01.PrintRune(' ')
+						z01.PrintRune(c)
+						z01.PrintRune(d)
+						last := !(a == '9' && b == '8' && c == '9' && d == '9')
+						if last {
+							z01.PrintRune(',')
+							z01.PrintRune(' ')
+						}
+					}
+				}
 			}
 		}
 	}
 	z01.PrintRune('\n')
+}
+
+func main() {
+	PrintComb2()
 }
