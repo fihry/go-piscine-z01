9d1ff77f5f698417ad20eb9132e2aa68dde44c5e
Christopher  Fremond <dev@01-edu.org>
2024-01-12 11:43:22 +0000

    Add 'atoi.go'

---
+++ atoi.go
@@ -0,0 +1,21 @@
+package piscine
+func Atoi(s string) int {
+  res:=0
+  sign:=1
+  for i:=0; i<=len(s)-1;i++{
+    if s[i]=='-'{
+      sign = -1
+      i++
+    }else if s[i]=='+'{
+      i++
+    }
+    if s[i]>='1' && s[i]<='9'{
+      res = res*10 + int(s[i]-'0')
+    }else if s[i] == '0'{
+      continue
+    }else{
+      return 0
+    }
+  }
+  return res*sign
+}
