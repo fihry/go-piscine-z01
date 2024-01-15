d9a33c71f57d41ffb9ee6d5e4617602b00a19bb1
melfihry <fihrylara@gmail.com>
2024-01-15 16:31:47 +0000

    findnextprime.go

---
+++ findnextprime.go
@@ -0,0 +1,27 @@
+package piscine
+
+func IsPrime(nb int) bool {
+	if nb <= 1 {
+		return false
+	}
+	for i := 2; i < nb; i++ {
+		if nb%i == 0 {
+			return false
+		}
+	}
+	return true
+}
+func FindNextPrime(nb int) int {
+	var res int
+	if IsPrime(nb) == true {
+		return nb
+	}
+	for i := 1; nb >= i; i++ {
+		if nb%i != 0 {
+			res = nb
+			break
+		}
+		nb += 1
+	}
+	return res
+}
