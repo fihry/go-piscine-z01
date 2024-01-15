2157f6b190266e585405e2c1b91867e518d20f9e
melfihry <fihrylara@gmail.com>
2024-01-15 09:26:44 +0000

    sortintegertable.go & atoi.go

--- atoi.go
+++ atoi.go
@@ -1,24 +1,23 @@
-package piscine 
+package piscine
+
 func Atoi(s string) int {
-    sign := 1
-    var result int
-    for i := 0; i < len(s); i++ {
-        if s[i] == '-' {
-            sign = -1
-          i++
-        } else if s[i] == '+' {
-            i++
-        }
-      for ;i< len(s); i++{
-          if s[i] >= '0' && s[i] <= '9' {
-            digit := int(s[i] - '0')
-            result = result*10 + digit
-        } else {
-            return 0
-        }
-      }
-    }
-  
-    return result * sign
-  
+	sign := 1
+	var result int
+	for i := 0; i < len(s); i++ {
+		if s[i] == '-' {
+			sign = -1
+			i++
+		} else if s[i] == '+' {
+			i++
+		}
+		for ; i < len(s); i++ {
+			if s[i] >= '0' && s[i] <= '9' {
+				digit := int(s[i] - '0')
+				result = result*10 + digit
+			} else {
+				return 0
+			}
+		}
+	}
+	return result * sign
 }
--- sortintegertable.go
+++ sortintegertable.go
@@ -1,8 +1,8 @@
 package piscine
 
 func SortIntegerTable(table []int) {
-	s := len(table)
-	for j := s - 1; j >= 0; j-- {
+	size := len(table)
+	for j := size - 1; j >= 0; j-- {
 		for i := 0; i < j; i++ {
 			if table[i] > table[i+1] {
 				table[i], table[i+1] = table[i+1], table[i]
