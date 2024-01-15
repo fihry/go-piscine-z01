6347d24cb32ca1544169391f606fba27858b998f
melfihry <fihrylara@gmail.com>
2024-01-15 15:15:57 +0000

    isprime.go

--- isprime.go
+++ isprime.go
@@ -3,13 +3,10 @@ package piscine
 func IsPrime(nb int) bool {
 	if nb <= 1 {
 		return false
-	} else if nb > 2 {
-		for i := 2; i < nb; i++ {
-			if nb%i == 0 {
-				return false
-			} else {
-				return true
-			}
+	}
+	for i := 2; i < nb; i++ {
+		if nb%i == 0 {
+			return false
 		}
 	}
 	return true
