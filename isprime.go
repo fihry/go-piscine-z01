81d27fbbf09669d52cb6798b088207f50c75c738
melfihry <fihrylara@gmail.com>
2024-01-15 12:43:09 +0000

    isprime.go

---
+++ isprime.go
@@ -0,0 +1,23 @@
+package main
+
+import "fmt"
+
+func IsPrime(nb int) bool {
+	var res bool
+	if nb > 1 {
+		for i := 2; i < nb; i++ {
+			if nb%i != 0 {
+				res = true
+			} else {
+				return false
+			}
+		}
+		return res
+	} else if nb < 2 {
+		return false
+	}
+	return res
+}
+func main() {
+	fmt.Println(IsPrime(17))
+}
