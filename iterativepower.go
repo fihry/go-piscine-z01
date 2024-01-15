a8257159af2ab0f75ff2bb550965c1b50e71fe02
melfihry <fihrylara@gmail.com>
2024-01-15 11:34:30 +0000

    iterativepower.go

---
+++ iterativepower.go
@@ -0,0 +1,15 @@
+package piscine
+
+func IterativePower(nb int, power int) int {
+	if power > 0 {
+		res := nb
+		for i := 1; power >= i; i++ {
+			res = nb * power
+		}
+		return res
+
+	} else {
+		return 0
+	}
+
+}
