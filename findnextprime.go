9bbd06f0a03a966c5b958e6fcf7492221e7dd94d
melfihry <fihrylara@gmail.com>
2024-01-16 10:28:13 +0000

    findnextprime.go

--- findnextprime.go
+++ findnextprime.go
@@ -3,8 +3,10 @@ package piscine
 func NbIsPrime(nb int) bool {
 	if nb <= 1 {
 		return false
+	} else if nb%2 == 0 {
+		return false
 	}
-	for i := 2; i < nb; i++ {
+	for i := 2; i*i <= nb; i += 2 {
 		if nb%i == 0 {
 			return false
 		}
@@ -13,15 +15,17 @@ func NbIsPrime(nb int) bool {
 }
 
 func FindNextPrime(nb int) int {
+	var res int
 	if NbIsPrime(nb) == true {
 		return nb
 	} else if nb <= 2 {
 		return 2
 	}
-	for i := 2; i <= nb; i++ {
-		if NbIsPrime(nb) != true {
-			nb++
+	for i := 1; i <= nb; i++ {
+		if NbIsPrime(nb) == true {
+			return nb
 		}
+		nb += 1
 	}
-	return nb
+	return res
 }
