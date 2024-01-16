8e49739d3356f67fce63d9f076b0edeb5d468c80
melfihry <fihrylara@gmail.com>
2024-01-16 13:35:02 +0000

    nrune.go

--- nrune.go
+++ nrune.go
@@ -1,10 +1,9 @@
 package piscine
 
 func NRune(s string, n int) rune {
-	var re rune
 	if n >= 0 && n <= len(s) {
 		res := []rune(s)
-		re = res[n-1]
+		return res[n-1]
 	}
-	return re
+	return 0
 }
