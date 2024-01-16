ec5bed5550c58f9ce8bfb297e773bb242802143f
melfihry <fihrylara@gmail.com>
2024-01-16 16:24:18 +0000

    index.go

--- index.go
+++ index.go
@@ -1,13 +1,13 @@
 package piscine
 
 func Index(s string, toFind string) int {
-	if len(toFind) != 0 {
-		for i := 0; i <= len(s)-1; i++ {
-			if s[i] == toFind[0] {
-				return i
-			}
-		}
-		return -1
+	if len(toFind) == 0 {
+		return 0
 	}
-	return 0
+	for i := 0; i <= len(s)-1; i++ {
+		if s[i] == toFind[0] {
+			return i
+		}
+	}
+	return -1
 }
