ce78c9bf2ea0a73ed6a215286e6d3619d0a7c5d7
melfihry <fihrylara@gmail.com>
2024-01-16 16:17:34 +0000

    index.go

--- index.go
+++ index.go
@@ -1,10 +1,24 @@
-package piscine
+package main
+
+import (
+	"fmt"
+	// "piscine"
+)
+
+func main() {
+	fmt.Println(Index("Hello!", "l"))
+	fmt.Println(Index("Salut!", "alu"))
+	fmt.Println(Index("", ""))
+}
 
 func Index(s string, toFind string) int {
-	for i := 0; i <= len(s)-1; i++ {
-		if s[i] == toFind[0] {
-			return i
+	if len(toFind) != 0 {
+		for i := 0; i <= len(s)-1; i++ {
+			if s[i] == toFind[0] {
+				return i
+			}
 		}
+		return -1
 	}
-	return -1
+	return 0
 }
