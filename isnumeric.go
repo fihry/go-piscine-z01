3860491649401afd3d420c843f46cbd40f42439b
melfihry <fihrylara@gmail.com>
2024-01-17 09:58:11 +0000

    isnumeric.go

--- isnumeric.go
+++ isnumeric.go
@@ -1,8 +1,8 @@
 package piscine
 
 func IsNumeric(s string) bool {
-	for i := range s {
-		if s[i] < '0' || s[i] > '9' {
+	for j := range s {
+		if s[j] < '0' || s[j] > '9' {
 			return false
 		}
 	}
