59d5bd73e339376cd1905691eaa169b1e29aad85
melfihry <fihrylara@gmail.com>
2024-01-17 09:47:45 +0000

    isalpha.go

--- isalpha.go
+++ isalpha.go
@@ -2,8 +2,10 @@ package piscine
 
 func IsAlpha(s string) bool {
 	for i := range s {
-		if s[i] < 'A' || s[i] > 'Z' && s[i] < 'a' || s[i] > 'z' && s[i] < '0' || s[i] > '9' {
+		if (s[i] < 'A' || s[i] > 'Z') && (s[i] < 'a' || s[i] > 'z') && (s[i] < '0' || s[i] > '9') {
 			return false
+		} else if s[i] >= '0' && s[i] <= '9' {
+			return true
 		}
 	}
 	return true
