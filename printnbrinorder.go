d0bc09ce53bbb45ed8ce37bd299701b208b2133a
melfihry <fihrylara@gmail.com>
2024-01-17 16:09:33 +0000

    printnbrinorder.go

--- printnbrinorder.go
+++ printnbrinorder.go
@@ -19,14 +19,18 @@ func PrintNbrInOrder(n int) {
 		digits = append(digits, digit)
 		n /= 10
 	}
+	SortNb(digits)
+	for _, v := range digits {
+		z01.PrintRune(rune(v + '0'))
+	}
+}
+
+func SortNb(digits []int) {
 	for i := 0; i < len(digits); i++ {
-		for j := i + 1; j < len(digits); j++ {
-			if digits[i] > digits[j] {
+		for j := 0; j < len(digits)-i-1; j++ {
+			if digits[j] > digits[j+1] {
 				digits[j], digits[j+1] = digits[j+1], digits[j]
 			}
 		}
-	}
-	for _, v := range digits {
-		z01.PrintRune(rune(v + '0'))
 	}
 }
