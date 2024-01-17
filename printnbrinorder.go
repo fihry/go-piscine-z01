329aae4bc12d2abe3648245fede973f3294d4fc9
melfihry <fihrylara@gmail.com>
2024-01-17 15:55:27 +0000

    printnbrinorder.go

--- printnbrinorder.go
+++ printnbrinorder.go
@@ -1,36 +1,28 @@
 package piscine
 
-import "github.com/01-edu/z01"
+import (
+	"github.com/01-edu/z01"
+)
 
 func PrintNbrInOrder(n int) {
-	if n == -9223372036854775808 {
-		z01.PrintRune('-')
-		z01.PrintRune('9')
-		z01.PrintRune('2')
-		z01.PrintRune('2')
-		z01.PrintRune('3')
-		z01.PrintRune('3')
-		z01.PrintRune('7')
-		z01.PrintRune('2')
-		z01.PrintRune('0')
-		z01.PrintRune('3')
-		z01.PrintRune('6')
-		z01.PrintRune('8')
-		z01.PrintRune('5')
-		z01.PrintRune('4')
-		z01.PrintRune('7')
-		z01.PrintRune('7')
-		z01.PrintRune('5')
-		z01.PrintRune('8')
-		z01.PrintRune('0')
-		z01.PrintRune('8')
-	} else if n < 0 {
-		z01.PrintRune('-')
-		n = -n
-	} else if n > 9 {
-		z01.PrintRune(rune(n%10) + '0')
-		PrintNbrInOrder(n / 10)
-	} else {
-		z01.PrintRune(rune(n%10) + '0')
+	digits := []int{}
+
+	if n == 0 {
+		z01.PrintRune(rune(n + '0'))
+	}
+	for n > 0 {
+		digit := n % 10
+		digits = append(digits, digit)
+		n /= 10
+	}
+	for i := 0; i < len(digits); i++ {
+		for j := i + 1; j < len(digits); j++ {
+			if digits[i] > digits[j] {
+				digits[j], digits[j+1] = digits[j+1], digits[j]
+			}
+		}
+	}
+	for _, digit := range digits {
+		z01.PrintRune(rune(digit + '0'))
 	}
 }
