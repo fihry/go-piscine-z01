f45687353bc9dfafdffb3deb43032c4ac081f28d
melfihry <fihrylara@gmail.com>
2024-01-17 08:59:58 +0000

    isupper.go

--- isupper.go
+++ isupper.go
@@ -1,13 +1,10 @@
 package piscine
 
 func IsUpper(s string) bool {
-	var res bool
 	for i := range s {
-		if s[i] >= 65 && s[i] <= 90 {
-			res = true
-		} else {
-			res = false
+		if s[i] < 65 || s[i] > 90 {
+			return false
 		}
 	}
-	return res
+	return true
 }
