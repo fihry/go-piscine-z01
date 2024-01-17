8b24c912dcae23fb7a7fb6953b2a4e96a52dcbaa
melfihry <fihrylara@gmail.com>
2024-01-17 09:51:18 +0000

    isalpha.go

--- isalpha.go
+++ isalpha.go
@@ -4,8 +4,6 @@ func IsAlpha(s string) bool {
 	for i := range s {
 		if (s[i] < 'A' || s[i] > 'Z') && (s[i] < 'a' || s[i] > 'z') && (s[i] < '0' || s[i] > '9') {
 			return false
-		} else if s[i] >= '0' && s[i] <= '9' {
-			return true
 		}
 	}
 	return true
