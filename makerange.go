87c9f94ddb76e2ea35ed9191b9ba0edec8ffebda
melfihry <fihrylara@gmail.com>
2024-01-18 17:11:37 +0000

    makerange.go

---
+++ makerange.go
@@ -0,0 +1,12 @@
+package piscine
+
+func MakeRange(min, max int) []int {
+	if min > max {
+		return nil
+	}
+	array := make([]int, max-min)
+	for i := range array {
+		array[i] = min + i
+	}
+	return array
+}
