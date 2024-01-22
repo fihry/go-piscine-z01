19f69132cd604e29e2f41144d64fda6893856415
melfihry <fihrylara@gmail.com>
2024-01-22 13:57:13 +0000

    point

--- point/main.go
+++ point/main.go
@@ -5,26 +5,48 @@ import (
 )
 
 type point struct {
-	x string
-	y string
+	x int
+	y int
 }
 
 func setPoint(ptr *point) {
-	ptr.x = "42"
-	ptr.y = "21"
+	ptr.x = -42
+	ptr.y = 21
+}
+
+func ItoaConv(i int) string {
+	var a string
+	isNegative := false
+	if i < 0 {
+		isNegative = true
+		i = i * -1
+	}
+	if i == 0 {
+		return "0"
+	}
+	for i != 0 {
+		a = string(i%10+'0') + a
+		i = i / 10
+	}
+	if isNegative {
+		a = "-" + a
+	}
+	return a
 }
 
 func main() {
 	points := &point{
-		x: "0",
-		y: "0",
+		x: 0,
+		y: 0,
 	}
 	setPoint(points)
+	xStr := ItoaConv(points.x)
+	yStr := ItoaConv(points.y)
 	z01.PrintRune('x')
 	z01.PrintRune(' ')
 	z01.PrintRune('=')
 	z01.PrintRune(' ')
-	for _, r := range points.x {
+	for _, r := range xStr {
 		z01.PrintRune(r)
 	}
 	z01.PrintRune(',')
@@ -33,7 +55,7 @@ func main() {
 	z01.PrintRune(' ')
 	z01.PrintRune('=')
 	z01.PrintRune(' ')
-	for _, r := range points.y {
+	for _, r := range yStr {
 		z01.PrintRune(r)
 	}
 	z01.PrintRune('\n')
