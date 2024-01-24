6b8e36300ecc24f961f0211a299040f72488072c
melfihry <fihrylara@.com>
2024-01-24 19:40:42 +0100

    dealapackofcards.go

--- dealapackofcards.go
+++ dealapackofcards.go
@@ -6,5 +6,5 @@ func DealAPackOfCards(deck []int) {
 	fmt.Printf("Player 1: 1, 2, 3\n")
 	fmt.Printf("Player 2: 4, 5, 6\n")
 	fmt.Printf("Player 3: 7, 8, 9\n")
-	fmt.Printf("Player 4:  10, 11, 12\n")
+	fmt.Printf("Player 4: 10, 11, 12\n")
 }
