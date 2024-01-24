8eb1a02e00268d9891d8c30f6fd566ce75915325
melfihry <fihrylara@.com>
2024-01-24 21:11:55 +0100

    stringtointslice.go

--- stringtointslice.go
+++ stringtointslice.go
@@ -3,9 +3,7 @@ package piscine
 func StringToIntSlice(str string) []int {
 	res := []int{}
 	for i := 0; i < len(str); i++ {
-		if str[i] >= 'a' && str[i] <= 'z' || str[i] >= 'A' && str[i] <= 'Z' {
-			res = append(res, int(str[i]))
-		}
+		res = append(res, int(str[i]))
 	}
 	return res
 }
