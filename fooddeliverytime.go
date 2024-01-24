f6219ad06d846710a23099f22663c5be95409132
melfihry <fihrylara@gmail.com>
2024-01-24 14:17:15 +0100

    fooddeliverytime.go

--- fooddeliverytime.go
+++ fooddeliverytime.go
@@ -1,14 +1,17 @@
 package piscine
 
 // burger takes 15 min, chips takes 10 min and nuggets takes 12 min
-type food struct {
-	preptime int
-}
-
-var foods = map[string]food{
-	"burger": {15}, "chips": {10}, "nuggets": {12},
-}
 
 func FoodDeliveryTime(order string) int {
-	return foods[order].preptime
+	if order == "burger" {
+		return 15
+	}
+	if order == "chips" {
+		return 10
+	}
+	if order == "nuggets" {
+		return 12
+	} else {
+		return 404
+	}
 }
