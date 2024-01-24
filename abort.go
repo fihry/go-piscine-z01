5afec416f945f7b28e13502bb42ab6c7df1dee33
melfihry <fihrylara@gmail.com>
2024-01-24 16:08:34 +0100

    abort.go

---
+++ abort.go
@@ -0,0 +1,13 @@
+package piscine
+
+func Abort(a, b, c, d, e int) int {
+	arr := []int{a, b, c, d, e}
+	for i := 0; i < len(arr)-1; i++ {
+		for j := i + 1; j < len(arr); j++ {
+			if arr[j] < arr[i] {
+				arr[j], arr[i] = arr[i], arr[j]
+			}
+		}
+	}
+	return arr[2]
+}
