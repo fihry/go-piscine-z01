9bd3bafa03215eb5c70dccfeff0a4d2cf3034c38
melfihry <fihrylara@.com>
2024-01-25 09:04:47 +0100

    rockandroll.go

---
+++ rockandroll.go
@@ -0,0 +1,17 @@
+package piscine
+
+func RockAndRoll(n int) string {
+	if n < 0 {
+		return "error: number is negative\n"
+	}
+	switch n > 0 {
+	case n%2 == 0 && n%3 == 0:
+		return "rock and roll\n"
+	case n%2 == 0:
+		return "rock\n"
+	case n%3 == 0:
+		return "roll\n"
+	default:
+		return "error: non divisible\n"
+	}
+}
