f038977ed9d5ebc0fda715d03998d69d7060c63b
fihry <fihrylara@gmail.com>
2024-01-30 11:56:00 +0100

    listat.go

--- listat.go
+++ listat.go
@@ -4,11 +4,11 @@ func ListAt(l *NodeL, pos int) *NodeL {
 	count := 0
 	v := l
 	for v != nil {
-		v = v.Next
-		count++
 		if count == pos {
 			return v
 		}
+		v = v.Next
+		count++
 	}
 	return nil
 }
