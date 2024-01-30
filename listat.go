548a0f048011954d1b3e51ac9fd0f6ec4835ba66
fihry <fihrylara@gmail.com>
2024-01-30 11:42:59 +0100

    listat.go

--- listat.go
+++ listat.go
@@ -1,12 +1,12 @@
 package piscine
 
 func ListAt(l *NodeL, pos int) *NodeL {
-	count := 0
+	counter := 0
 	v := l
 	for v != nil {
 		v = v.Next
-		count++
-		if count == pos {
+		counter++
+		if counter == pos {
 			return v
 		}
 	}
