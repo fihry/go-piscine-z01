c947e84ce53eea043b0dfb4f922f64e10e8f6e60
fihry <fihrylara@gmail.com>
2024-02-01 12:57:02 +0100

    btreesearchitem.go

---
+++ btreesearchitem.go
@@ -0,0 +1,15 @@
+package piscine
+
+func BTreeSearchItem(root *TreeNode, elem string) *TreeNode {
+	if root == nil {
+		return nil
+	}
+	if root.Data == elem {
+		return root
+	}
+	if root.Data > elem {
+		return BTreeSearchItem(root.Left, elem)
+	} else {
+		return BTreeSearchItem(root.Right, elem)
+	}
+}
